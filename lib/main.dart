f758f8a38ce4f986d03bef85c1c388a5b44d8726
Uziel Cornejo Olivares <61181141+RR-UZI@users.noreply.github.com>
2023-10-30 21:50:58 -0600

    Creacion de la plantilla

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
 
 void main() {
   runApp(const FlowerRecognitionApp());
@@ -15,6 +19,31 @@ class FlowerRecognitionApp extends StatelessWidget {
     );
   }
 }
+
+class FlowerRecognitionHome extends StatefulWidget {
+  const FlowerRecognitionHome({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FlowerRecognitionHomeState createState() => _FlowerRecognitionHomeState();
+}
+
+class _FlowerRecognitionHomeState extends State<FlowerRecognitionHome> {
+  // ignore: unused_field
+
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('PlanFI'),
+      backgroundColor: Colors.green,
+    ),
+    ),
+}
+
+}
+
 
 
 
