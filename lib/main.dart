9ee3f10d8ff6217883c3f8f0a58e7183318b4551
Carlos-Garcia09 <Carlos.GMadrigal@alumno.udg.mx>
2023-10-30 21:57:15 -0600

    Agregamos widget de agregar imagen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const FlowerRecognitionApp());
@@ -16,6 +19,51 @@ class FlowerRecognitionApp extends StatelessWidget {
   }
 }
 
+class FlowerRecognitionHome extends StatefulWidget {
+  const FlowerRecognitionHome({super.key});
 
+  @override
+  // ignore: library_private_types_in_public_api
+  _FlowerRecognitionHomeState createState() => _FlowerRecognitionHomeState();
+}
+
+class _FlowerRecognitionHomeState extends State<FlowerRecognitionHome> {
+  // ignore: unused_field
+  final picker = ImagePicker();
+  late PickedFile _image = PickedFile('');
+
+  
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> getImageFromGallery() async {
+    
+  }
+
+  @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('PlanFI'),
+      backgroundColor: Colors.green,
+    ),
+    body: Center(
+      // ignore: unnecessary_null_comparison
+      child: _image == null
+          
+          : Image.file(File(_image.path)),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: getImageFromGallery,
+      tooltip: 'Seleccionar imagen',
+      child: const Icon(Icons.add_a_photo),
+    ),
+  );
+}
+
+}
 
 
